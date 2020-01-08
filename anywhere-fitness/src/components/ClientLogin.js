e2dfa07461e7247db505604d9af3b4a61275f9ff
playtez <aaron.pleitez12@gmail.com>
2020-01-08 10:49:54 -0600

    cleaned up files on Client Page

--- anywhere-fitness/src/components/ClientLogin.js
+++ anywhere-fitness/src/components/ClientLogin.js
@@ -1,82 +1,86 @@
 // lydie this is where you build the login form
 // implement material ui
 
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Container, Typography, TextField, Button, Grid, Link } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
+import React, { useState } from "react";
+import axios from "axios";
+import {
+  Container,
+  Typography,
+  TextField,
+  Button,
+  Grid,
+  Link
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 export default function ClientLogin() {
-    const styling = useStyles();
-    const [message, setMessage] = useState('')
+  const styling = useStyles();
+  const [message, setMessage] = useState("");
 
-    const [fields, handleFieldChange] = useFormFields({
-        username: "",
-        password: ""
-    });
+  const [fields, handleFieldChange] = useFormFields({
+    username: "",
+    password: ""
+  });
 
-    function useFormFields(initialState) {
-        const [fields, setValues] = useState(initialState);
-        return [
-            fields,
-            function (event) {
-                setValues({
-                    ...fields,
-                    [event.target.id]: event.target.value
-                });
-            }
-        ];
-    }
+  function useFormFields(initialState) {
+    const [fields, setValues] = useState(initialState);
+    return [
+      fields,
+      function(event) {
+        setValues({
+          ...fields,
+          [event.target.id]: event.target.value
+        });
+      }
+    ];
+  }
 
-    function validateForm() {
-        return fields.username.length > 0 && fields.password.length > 0;
-    }
+  function validateForm() {
+    return fields.username.length > 0 && fields.password.length > 0;
+  }
 
+  function handleSubmit(values, tools) {
+    axios
+      .post("http://localhost:4000/login", values)
+      .then(response => {
+        setMessage(response.data.message);
+        tools.resetForm();
+      })
+      .catch();
+  }
 
-    function handleSubmit(values, tools) {
-        axios.post('http://localhost:4000/login', values)
-            .then(response => {
-                setMessage(response.data.message);
-                tools.resetForm();
-            })
-            .catch()
-    };
+  return (
+    <Container className={styling.root}>
+      <div>
+        <Typography variant="h3" component="h1" align="center">
+          Client Login
+        </Typography>
 
-    return (
-        <Container className={styling.root}>
-            <div>
-                <Typography variant='h3' component='h1' align='center'>
-                    Client Login
-            </Typography>
+        <form className={styling.form} onSubmit={handleSubmit}>
+          <label />
+          <TextField
+            required
+            id="username"
+            type="text"
+            name="username"
+            label="username"
+            fullWidth
+            onChange={handleFieldChange}
+            value={fields.username}
+          ></TextField>
 
-                <form className={styling.form} onSubmit={handleSubmit}>
-                    <label />
-                    <TextField
-                        required
-                        id='username'
-                        type='text'
-                        name='username'
-                        label='username'
-                        fullWidth
-                        onChange={handleFieldChange}
-                        value={fields.username}
-                    >
-                    </TextField>
-
-                    <label />
-                    <TextField
-                        required
-                        id='password'
-                        type='password'
-                        name='password'
-                        label='Password'
-                        fullWidth
-                        onChange={handleFieldChange}
-                        value={fields.password}
-                    >
-                    </TextField>
-
-        <form className={styling.form}>
+          <label />
+          <TextField
+            required
+            id="password"
+            type="password"
+            name="password"
+            label="Password"
+            fullWidth
+            onChange={handleFieldChange}
+            value={fields.password}
+          ></TextField>
+          <label />
           <TextField
             id="email"
             label="Email Address"
@@ -84,25 +88,15 @@ export default function ClientLogin() {
             fullWidth
           ></TextField>
 
-
-                    <Button className={styling.button} disabled={!validateForm()}
-                        type='submit'
-                        variant="contained"
-                        fullWidth
-                    >
-                        Login
-                </Button>
-
-
           <Button
             className={styling.button}
+            disabled={!validateForm()}
             type="submit"
             variant="contained"
             fullWidth
           >
             Login
           </Button>
-
 
           <Grid>
             <Grid item xs>
@@ -116,23 +110,20 @@ export default function ClientLogin() {
   );
 }
 
-                        
-
 const useStyles = makeStyles(theme => ({
-    root: {
-        display: "flex",
-        flexDirection: 'column',
-        alignItems: 'center',
-        marginTop: theme.spacing(7),
-    },
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: theme.spacing(7)
+  },
 
-    form: {
-        width: "100%",
-        marginTop: theme.spacing(1),
-    },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1)
+  },
 
-    button: {
-        margin: theme.spacing(3, 0, 3),
-    },
+  button: {
+    margin: theme.spacing(3, 0, 3)
+  }
 }));
-
