2d8166a910291028ef64fc3af8f1cd18a9aca11f
lydiecherilus <lydiecherilus@yahoo.fr>
2020-01-09 14:25:32 -0500

    Client Login form completed - Event listener and Axios get request added

--- anywhere-fitness/src/components/ClientLogin.js
+++ anywhere-fitness/src/components/ClientLogin.js
@@ -1,8 +1,10 @@
-// lydie this is where you build the login form
-// implement material ui
+// lydie this is where you build the login form -- implement material ui
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
+import { Card, CardTitle, CardImg } from "reactstrap";
+import { makeStyles } from "@material-ui/core/styles";
 import {
   Container,
   Typography,
@@ -10,12 +12,16 @@ import {
   Button,
   Grid
 } from "@material-ui/core";
-import { Link } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
+import useUser from "../context/useUser";
 
-export default function ClientLogin() {
+
+export default function ClientLogin(props) {
+
+  const { user, clientLogin } = useUser();
+  const { data } = user;
+  console.log({ data });
+  const [message, setMessage] = useState('')
   const styling = useStyles();
-  const [message, setMessage] = useState("");
 
   const [fields, handleFieldChange] = useFormFields({
     username: "",
@@ -39,69 +45,117 @@ export default function ClientLogin() {
     return fields.username.length > 0 && fields.password.length > 0;
   }
 
-  function handleOnSubmit() {
-    return ('Welcome Back')
-  }
+  function handleSubmit(values, tools) {
+    axios.post('http://localhost:3000/login', values)
+      .then(response => {
+        setMessage(response.data.message);
+        tools.resetForm();
+      })
+      .catch()
+  };
+
+
+  // implementing axios get request
+  const [dogs, setDogs] = useState([]);
+  const [breed, setBreed] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`https://dog.ceo/api/breed/${breed}/images/random/1`)
+      .then(response => {
+        const RandomDogs = response.data.message;
+        setDogs(RandomDogs);
+      })
+      .catch(error => {
+
+      });
+  }, [breed]);
 
   return (
-    <Container className={styling.root}>
-      <div>
-        <Typography variant="h3" component="h1" align="center">
-          Client Login
-        </Typography>
+    <div>
+      <Button color="primary" onClick={() => setBreed("chihuahua")}>
+        Chihuahua
+      </Button>
+      <button onClick={() => setBreed("african")}>African</button>
+      <button onClick={() => setBreed("chow")}>Chow</button>
+      <button onClick={() => setBreed("beagle")}>Beagle</button>
+      <button onClick={() => setBreed("boxer")}>Boxer</button>
+      <button onClick={() => setBreed("husky")}>Husky</button>
+      <button onClick={() => setBreed("dhole")}>Dhole</button>
+      <button onClick={() => setBreed("cairn")}>Cairn</button>
+      <button onClick={() => setBreed("shiba")}>Shiba</button>
+      <button onClick={() => setBreed("mix")}>Mix</button>
 
-        <form className={styling.form} onSubmit={handleOnSubmit()}>
-          <label />
-          <TextField
-            required
-            id="username"
-            type="text"
-            name="username"
-            label="username"
-            fullWidth
-            onChange={handleFieldChange}
-            value={fields.username}
-          ></TextField>
-
-          <label />
-          <TextField
-            required
-            id="password"
-            type="password"
-            name="password"
-            label="Password"
-            fullWidth
-            onChange={handleFieldChange}
-            value={fields.password}
-          ></TextField>
-          <label />
-          <TextField
-            id="email"
-            label="Email Address"
-            name="email"
-            fullWidth
-          ></TextField>
-
-          <Button
-            className={styling.button}
-            disabled={!validateForm()}
-            type="submit"
-            variant="contained"
-            fullWidth
-          >
-            Login
-          </Button>
-
-          <Grid>
-            <Grid item xs>
-              <Link to="/register-form">Don't have an account? Sign Up </Link>
-            </Grid>
-            <Link to="/instructor-login">Instructor Login</Link>
-          </Grid>
-        </form>
+      <div className="entry">
+        {dogs.map(entry => {
+          return <DogCard key={entry} breed={breed} imgUrl={entry} />;
+        })}
       </div>
 
-    </Container>
+
+      <Container className={styling.root}>
+        <div>
+          <Typography variant="h4" component="h1" align="center">
+            Member Login
+        </Typography>
+
+          <form className={styling.form} onSubmit={handleSubmit}>
+            <label />
+            <TextField
+              required
+              id="username"
+              type="text"
+              name="username"
+              label="username"
+              fullWidth
+              autoComplete="username"
+              onChange={handleFieldChange}
+              value={fields.username}
+            ></TextField>
+
+            <label />
+            <TextField
+              required
+              id="password"
+              type="password"
+              name="password"
+              label="Password"
+              fullWidth
+              autoComplete="current-password"
+              onChange={handleFieldChange}
+              value={fields.password}
+            ></TextField>
+            <label />
+            <TextField
+              id="email"
+              label="Email Address"
+              name="email"
+              fullWidth
+            ></TextField>
+
+            <Button
+              className={styling.button}
+              disabled={!validateForm()}
+              type="submit"
+              variant="contained"
+              fullWidth
+            >
+              Login
+          </Button>
+
+            <Grid>
+              <Grid item xs>
+                <Link to="/register-form">Don't have an account? Sign Up </Link>
+              </Grid>
+              <Link to="/instructor-login">Instructor Login</Link>
+            </Grid>
+          </form>
+        </div>
+
+        <h5>Love dogs? click on a breed name to see a new dog picture each time!</h5>
+
+      </Container>
+    </div>
   );
 }
 
@@ -110,12 +164,11 @@ const useStyles = makeStyles(theme => ({
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    marginTop: theme.spacing(7)
+    marginTop: theme.spacing(2)
   },
 
   form: {
     width: "100%",
-    marginTop: theme.spacing(1)
   },
 
   button: {
@@ -129,3 +182,15 @@ const formColorChange = document.querySelector('div')
 formColorChange.addEventListener('mouseenter', event => {
   event.target.style.color = 'purple';
 });
+
+const DogCard = props => {
+  return (
+    <div>
+      <Card>
+        <CardImg top width="20%" src={props.imgUrl} alt="random dog image" />
+        <CardTitle color="primary" style={{ color: "#555" }}>
+        </CardTitle>
+      </Card>
+    </div>
+  );
+};
